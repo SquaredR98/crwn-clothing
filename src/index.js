b15e474e2675951063c731d5aa4464d68996a435
SquaredR98 <yourfriendraviranjan@gmail.com>
2023-06-14 16:37:18 +0530

    Updated the code with context for easy state management

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.scss";
 import App from "./App";
+import { UserProvider } from "./contexts/user.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -15,7 +16,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,25 +1,27 @@
 import { initializeApp } from "firebase/app";
 
 /**
- * For authentication we need the following classes 
+ * For authentication we need the following classes
  * or methods in order to communicate with our auth
  * in the firebase.
  */
 import {
   getAuth,
-  signInWithRedirect,           // This redirects to another page for authentication
-  signInWithPopup,              // This creates popup instead of redirecting it to another page
+  signInWithRedirect, // This redirects to another page for authentication
+  signInWithPopup, // This creates popup instead of redirecting it to another page
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
 } from "firebase/auth";
 
 import {
-  getFirestore,                 // Instantiate the firestore db
-  doc,                          // Instantiate the document on which operation need to perform
-  getDoc,                       // Method to get data out of the instantiated document
-  setDoc                        // Method to set the data in the instantiated document
-} from 'firebase/firestore';
+  getFirestore, // Instantiate the firestore db
+  doc, // Instantiate the document on which operation need to perform
+  getDoc, // Method to get data out of the instantiated document
+  setDoc, // Method to set the data in the instantiated document
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyChjUsrlivuEo4fv0bhfKP7Ckz-clWIpUM",
@@ -30,39 +32,42 @@ const firebaseConfig = {
   appId: "1:72327552849:web:65eb9c56110ba880febd70",
 };
 
-const app = initializeApp(firebaseConfig);
-
+export const app = initializeApp(firebaseConfig);
 
 // In order to use google auth we need a provider as follows
 const provider = new GoogleAuthProvider();
 
-// Setting custom parameters so that we decide on how 
+// Setting custom parameters so that we decide on how
 // we want the provider to behave according to us.
 provider.setCustomParameters({
-  // This setting will provide user with promt to 
+  // This setting will provide user with promt to
   // select account for authentication.
-  prompt: 'select_account'
-})
+  prompt: "select_account",
+});
 
 export const auth = getAuth();
 
 // We must export the signUpWithPopup with auth and provider params
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
-export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, provider)
+    .then()
+    .catch((error) => error);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, provider);
 
 // Similar to mongoose models
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async(userAuth, additionalInfo) => {
-  // This creates the object irrespective of whether data 
+export const createUserDocumentFromAuth = async (userAuth, additionalInfo) => {
+  // This creates the object irrespective of whether data
   // is present in the db or not.
-  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userDocRef = doc(db, "users", userAuth.uid);
 
-  // So to make sure whether the above document exists in the 
+  // So to make sure whether the above document exists in the
   // db or not we use getDoc method as below
   const userSnapshot = await getDoc(userDocRef);
-  
-  if(!userSnapshot.exists()) {
+
+  if (!userSnapshot.exists()) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
 
@@ -71,25 +76,29 @@ export const createUserDocumentFromAuth = async(userAuth, additionalInfo) => {
         displayName,
         email,
         createdAt,
-        ...additionalInfo
-      })
+        ...additionalInfo,
+      });
     } catch (error) {
       console.log(error);
     }
   }
 
   return userDocRef;
-}
+};
 
-export const createAuthUserWithEmailAndPassword = async(email, password) => {
-  if( !email || !password) return;
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
 
   return await createUserWithEmailAndPassword(auth, email, password);
-}
+};
 
 export const signInAuthUserWithEmailAndPassword = async (email, password) => {
-  if(!email || !password) return;
+  if (!email || !password) return;
 
   return await signInWithEmailAndPassword(auth, email, password);
-}
+};
 
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) =>
+  callback && onAuthStateChanged(auth, callback);
