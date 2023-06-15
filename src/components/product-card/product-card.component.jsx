ce14385924afaf5f58a1fc2c31d9152f1b312ae7
SquaredR98 <yourfriendraviranjan@gmail.com>
2023-06-15 17:34:55 +0530

    Add count items to the shopping cart icon

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Button from '../button/button.component';
 
 import './product-card.styles.scss';
+import { CartContext } from '../../contexts/cart.context';
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+  const addProductToCart = () => addItemToCart(product);
   return (
     <div className='product-card-container'>
       <img src={imageUrl} alt={`${name}`} />
@@ -13,7 +16,7 @@ const ProductCard = ({ product }) => {
         <span className='name'>{name}</span>
         <span className='cost'>{price}</span>
       </div>
-      <Button buttonType='inverted'>Add to Cart</Button>
+      <Button buttonType='inverted' onClick={addProductToCart}>Add to Cart</Button>
     </div>
   )
 }
